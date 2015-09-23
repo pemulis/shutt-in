b1b0f7445c7c1b6a481db1952526c3c66f21650a
John Shutt <jds@riseup.net>
2015-09-22 21:05:24 -0700

    remove the residual rails app stuff

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Shuttin::Application.config.secret_token = '735fa0c6cb5d05e944532e2135a64a4a9da55ee0dd7b2d5411cdf6fcd07df6daa3b1c29d6750313946a83897f6614336b8bfc35885926e67cf6b20299f35cc9e'
--- config/initializers/task_scheduler.rb
+++
@@ -1,9 +0,0 @@
-require 'rufus/scheduler'
-scheduler = Rufus::Scheduler.new
-
-scheduler.every '20m' do
-  require 'net/http'
-  require 'uri'
-  url = 'http://shutt.in'
-  Net::HTTP.get_response(URI.parse(url))
-end
